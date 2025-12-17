af6f8c858a60e9378cc31c1d9dacea2c8a84b521
AryanRogye <161079183+AryanRogye@users.noreply.github.com>
2025-12-16 19:04:28 -0600

    removed unused code

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,9 +1,9 @@
 package app
 
 import (
+	"github.com/AryanRogye/ConfigFlipper/internal/models"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
-	"github.com/AryanRogye/ConfigFlipper/internal/models"
 )
 
 var appStyle = lipgloss.NewStyle().
@@ -12,6 +12,7 @@ var appStyle = lipgloss.NewStyle().
 	BorderForeground(lipgloss.Color("63"))
 
 type screen int
+
 const (
 	screenRoot screen = iota
 	screenCreateConfig
@@ -21,7 +22,7 @@ const (
 type model struct {
 	screen screen
 
-	root root
+	root               root
 	createConfigScreen createConfigScreen
 }
 
@@ -29,7 +30,7 @@ func InitialModel(config models.UserConfig) model {
 	return model{
 		screen: screenRoot,
 		/// Creating Root Screen
-		root: root {
+		root: root{
 			cursor: 0,
 			choices: [3]string{
 				"Create New Configuration",
@@ -73,7 +74,8 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		m.createConfigScreen.Update(msg, func(screen screen) {
 			m.screen = screen
 		})
-	default: break
+	default:
+		break
 	}
 
 	return m, nil
@@ -81,75 +83,11 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 func (m model) View() string {
 	switch m.screen {
-		case screenRoot:
-			return appStyle.Render(m.root.View())
-		case screenCreateConfig:
-			return appStyle.Render(m.createConfigScreen.View())
-		default:
-			return "Unkown\n"
+	case screenRoot:
+		return appStyle.Render(m.root.View())
+	case screenCreateConfig:
+		return appStyle.Render(m.createConfigScreen.View())
+	default:
+		return "Unkown\n"
 	}
 }
-
-// func createNewConfigScreen(pages *tview.Pages) tview.Primitive {
-//
-// 	backButton := tview.NewButton("Back").SetSelectedFunc(func() {
-// 		pages.SwitchToPage("root")
-// 	})
-//
-// 	flex := tview.NewFlex().SetDirection(tview.FlexRow).
-// 		AddItem(backButton, 1, 0, true)
-//
-// 	frame := tview.NewFrame(flex).
-// 		SetBorder(true).
-// 		SetTitle("Create New Configuration")
-//
-// 	return frame
-// }
-//
-// func selectNewConfigurationScreen(pages *tview.Pages) tview.Primitive {
-//
-// 	backButton := tview.NewButton("Back").SetSelectedFunc(func() {
-// 		pages.SwitchToPage("root")
-// 	})
-//
-// 	frame := tview.NewFrame(backButton).
-// 		SetBorder(true).
-// 		SetTitle("Select Configuration")
-//
-// 	flex := tview.NewFlex().SetDirection(tview.FlexRow).
-// 		AddItem(backButton, 1, 0, true).
-// 		AddItem(frame, 3, 0, false)
-// 	return flex
-// }
-//
-// func rootView(pages *tview.Pages) tview.Primitive {
-//
-// 	list := tview.NewList().
-// 		AddItem(ConfigFlipperOptions[0], "", 'a', func() {
-// 			pages.SwitchToPage("createNewConfigScreen")
-// 		}).
-// 		AddItem(ConfigFlipperOptions[1], "Select a new configuration, or create a new copy", 'b', func() {
-// 			pages.SwitchToPage("selectNewConfigurationScreen")
-// 		}).
-// 		AddItem(ConfigFlipperOptions[2], "", 'c', func() {})
-//
-// 	flex := tview.NewFlex().SetDirection(tview.FlexRow).
-// 		AddItem(list, 0, 1, true)
-//
-// 	return flex
-// }
-//
-// func Init() error {
-// 	app := tview.NewApplication()
-// 	pages := tview.NewPages()
-//
-// 	root := rootView(pages)
-// 	createNewConfigScreen := createNewConfigScreen(pages)
-// 	selectNewConfigurationScreen := selectNewConfigurationScreen(pages)
-//
-// 	pages.AddPage("root", root, true, true)
-// 	pages.AddPage("selectNewConfigurationScreen", selectNewConfigurationScreen, true, false)
-// 	pages.AddPage("createNewConfigScreen", createNewConfigScreen, true, false)
-//
-// 	return app.SetRoot(pages, true).Run()
-// }
